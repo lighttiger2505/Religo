ff35723e0d32f1570256fdf5485ea351b6a0fd91
lighttiger <lighttiger2505@gmail.com>
2017-01-14 14:46:52 +0900

    データモデルに追加日付を追加

--- religos/models.py
+++ religos/models.py
@@ -4,6 +4,7 @@ class Place(models.Model):
     name = models.CharField(max_length=200)
     phone_number = models.CharField(max_length=100)
     location = models.CharField(max_length=200)
+    add_date = models.DateTimeField(auto_now=False)
 
     def __str__(self):
         return self.name
