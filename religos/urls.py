c4889e15de14c6b4db368420d61205ef3f873bbe
lighttiger <lighttiger2505@gmail.com>
2017-01-14 14:38:10 +0900

    仮の画面構成を作成

--- religos/urls.py
+++ religos/urls.py
@@ -3,5 +3,12 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    # ex: /polls/
     url(r'^$', views.index, name='index'),
+    # ex: /polls/5/
+    url(r'^(?P<place_id>[0-9]+)/$', views.detail, name='detail'),
+    # ex: /polls/5/results/
+    url(r'^(?P<place_id>[0-9]+)/edit/$', views.edit, name='edit'),
+    # ex: /polls/5/vote/
+    url(r'^add/$', views.add, name='add'),
 ]
--- religos/views.py
+++ religos/views.py
@@ -2,3 +2,12 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the Religo index.")
+
+def detail(request, place_id):
+    return HttpResponse("You'er looking at place %s" %place_id)
+
+def edit(request, place_id):
+    return HttpResponse("You'er editing at place %s" %place_id)
+
+def add(request):
+    return HttpResponse("You'er adding at place")
