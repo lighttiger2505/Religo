433cf71f88b25b33eb386067d05e6a2a1f57fa5e
lighttiger <lighttiger2505@gmail.com>
2017-01-14 12:58:34 +0900

    モデルに説明を追加

--- religos/models.py
+++ religos/models.py
@@ -4,3 +4,6 @@ class Place(models.Model):
     name = models.CharField(max_length=200)
     phone_number = models.CharField(max_length=100)
     location = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
