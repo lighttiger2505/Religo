64ff5a93a512bfd9bc60e12a5914bde13b553916
lighttiger <lighttiger2505@gmail.com>
2017-01-21 22:54:08 +0900

    フォームの表示内容を変更

--- religos/forms.py
+++ religos/forms.py
@@ -8,6 +8,22 @@ class PlaceForm(ModelForm):
     class Meta:
         model = Place
         fields = ['name', 'phone_number', 'location']
+        labels = {
+            'name': '店名',
+            'phone_number': '電話番号',
+            'location': '住所',
+        }
+        help_texts = {
+            'name': '店名を入力してください。',
+            'phone_number': '電話番号を入力してください。例：xxxx-xx-xxxx',
+            'location': '住所を入力してください。例：◯◯県◯◯市◯◯町x-x-x',
+        }
+        error_messages = {
+            'name': {
+                'max_length': "This writer's name is too long.",
+                'required': "必須入力項目です。",
+            }
+        }
 
 
 class PhotoForm(forms.Form):
--- religos/models.py
+++ religos/models.py
@@ -3,9 +3,10 @@ from django.db import models
 
 class Place(models.Model):
     name = models.CharField(max_length=200)
-    phone_number = models.CharField(max_length=100, null=True)
-    location = models.CharField(max_length=200, null=True)
-    add_date = models.DateTimeField(auto_now=True)
+    phone_number = models.CharField(max_length=100, null=True, blank=True)
+    location = models.CharField(max_length=200, null=True, blank=True)
+    add_date = models.DateTimeField(auto_now_add=True)
+    update_date = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
